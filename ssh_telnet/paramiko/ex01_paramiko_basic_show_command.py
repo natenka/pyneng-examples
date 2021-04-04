8d5d50c7e4d2dc25acc47baa25953ce75b1abf4f
pyneng <pyneng.course@gmail.com>
2021-04-04 14:01:55 +0000

    add paramiko examples

--- ssh_telnet/paramiko/ex01_paramiko_basic_show_command.py
+++ ssh_telnet/paramiko/ex01_paramiko_basic_show_command.py
@@ -3,7 +3,7 @@ import time
 from pprint import pprint
 
 
-def cisco_send_show_command(
+def send_show_command(
     host, username, password, enable_pass, command, max_read=60000, pause=0.5
 ):
     client = paramiko.SSHClient()
@@ -27,14 +27,11 @@ def cisco_send_show_command(
         ssh.send(f"{command}\n")
         time.sleep(pause)
         output = ssh.recv(max_read)
-        return output.decode("utf-8")
+        return output.decode("utf-8").replace("\r\n", "\n")
 
 
 if __name__ == "__main__":
-    result = {}
     ip_list = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
     for ip in ip_list:
-        out = cisco_send_show_command(ip, "cisco", "cisco", "cisco", "ping 8.8.8.8")
-        result[ip] = out
-        break
-    pprint(result)
+        out = send_show_command(ip, "cisco", "cisco", "cisco", "sh ip int br")
+        pprint(out, width=120)
---
+++ ssh_telnet/paramiko/ex02_paramiko_read_until_prompt.py
@@ -0,0 +1,79 @@
+import time
+from pprint import pprint
+import socket
+import re
+
+import paramiko
+import yaml
+
+
+def _read_until_prompt(ssh, prompt, pause=0.2, max_read=60000):
+    command_output = ""
+    ssh.settimeout(5)
+    while True:
+        try:
+            time.sleep(pause)
+            part = ssh.recv(max_read).decode("utf-8")
+            command_output += part
+            if prompt in part:
+                break
+        except socket.timeout:
+            break
+    return command_output.replace("\r\n", "\n")
+
+
+def send_show_commands(
+    host, username, password, enable_pass, commands, max_read=60000, pause=0.5
+):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        client.connect(
+            hostname=host,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+            timeout=10
+        )
+    except socket.timeout:
+        print(f"Failed to connect to {host}")
+        return
+    except paramiko.SSHException as error:
+        print(f"{error} on {host}")
+        return
+
+    with client.invoke_shell() as ssh:
+        ssh.settimeout(2)
+
+        ssh.send("enable\n")
+        ssh.send(f"{enable_pass}\n")
+        time.sleep(pause)
+        ssh.recv(max_read)
+        ssh.send(f"terminal length 0\n")
+        time.sleep(pause)
+        read_output = ssh.recv(max_read).decode("utf-8")
+        match_prompt = re.search(r"\S+#", read_output)
+
+        if match_prompt:
+            prompt = match_prompt.group()
+        else:
+            prompt = "#"
+
+        if type(commands) == str:
+            commands = [commands]
+        result = {}
+        for cmd in commands:
+            ssh.send(f"{cmd}\n")
+            output = _read_until_prompt(ssh, prompt)
+            result[cmd] = output
+        return result
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+        for device in devices:
+            out = send_show_commands(**device, commands="sh ip int br")
+            pprint(out, width=120)
