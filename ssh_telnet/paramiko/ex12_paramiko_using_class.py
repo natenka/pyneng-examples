d0574e623a825847dfe4b4e614bc812290f96c35
pyneng <pyneng.course@gmail.com>
2021-04-04 14:55:56 +0000

    add paramiko class examples

---
+++ ssh_telnet/paramiko/ex12_paramiko_using_class.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import socket
+import paramiko
+import yaml
+from ex11_paramiko_class import ConnectSSH
+
+
+def send_show_command(device, command):
+    try:
+        with ConnectSSH(**device) as ssh:
+            output = ssh.send_command(command)
+            return output
+    except socket.timeout:
+        print(f"Failed to connect to {host}")
+    except paramiko.SSHException as error:
+        print(f"{error} on {host}")
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+        for dev in devices:
+            out = send_show_command(dev, command="sh ip int br")
+            pprint(out, width=120)
+
