29ac15c0a0fa970a3380d2004a0d808b8cd765ab
natenka <nataliya.samoylenko@gmail.com>
2022-08-08 06:51:56 +0000

    new concurrent.futures example: collect exceptions

--- concurrent_futures/ex16_netmiko_threads_submit_collect_exceptions_logging.py
+++ concurrent_futures/ex16_netmiko_threads_submit_collect_exceptions_logging.py
@@ -4,39 +4,51 @@ import logging
 
 from netmiko import Netmiko
 
-logging.getLogger("paramiko").setLevel(logging.WARNING)
-logging.getLogger("netmiko").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-logging.basicConfig(
-    format="{threadName} {asctime} {name} {levelname} {message}",
-    datefmt="%H:%M:%S",
-    style="{",
-    level=logging.DEBUG,
+### stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "{threadName} {asctime} {name} {levelname} {message}",
+     datefmt="%H:%M:%S", style="{"
 )
+console.setFormatter(formatter)
+
+logger.addHandler(console)
+
+### File
+logfile = logging.FileHandler("exceptions_report.log")
+logfile.setLevel(logging.DEBUG)
+formatter = logging.Formatter("{asctime} {threadName} {name} {levelname} {message}", style="{")
+logfile.setFormatter(formatter)
+
+logger.addHandler(logfile)
 
 
 def send_show(device_dict, command):
     device = device_dict.get("host")
-    logging.info(f">>> Connect to {device}")
-    with Netmiko(**device_dict) as conn:
-        conn.enable()
-        output = conn.send_command(command)
-        return output
+    logger.info(f">>> Connecting to {device}")
+    try:
+        with Netmiko(**device_dict) as conn:
+            conn.enable()
+            output = conn.send_command(command)
+            return output
+    except Exception as error:
+        logger.debug(f"Error on {device} {error}", exc_info=True) # exc_info добавляет traceback
 
 
 def send_cmd_to_all(devices, command, threads=10):
     ip_out_dict = {}
-    errors_on_devices = {}
     with ThreadPoolExecutor(max_workers=threads) as ex:
         task_queue = [ex.submit(send_show, dev, command=command) for dev in devices]
         for dev, future in zip(devices, task_queue):
             host = dev["host"]
-            exception = future.exception()
-            if exception:
-                errors_on_devices[host] = exception
-            else:
+            output = future.result()
+            if output:
                 ip_out_dict[host] = future.result()
-    return ip_out_dict, errors_on_devices
+    return ip_out_dict
 
 
 if __name__ == "__main__":
@@ -91,5 +103,6 @@ if __name__ == "__main__":
         },
     ]
     cmd = "sh run | i hostname"
-    correct, errors = send_cmd_to_all(devices, cmd)
-    pprint(errors)
+    result = send_cmd_to_all(devices, cmd)
+    pprint(result)
+
